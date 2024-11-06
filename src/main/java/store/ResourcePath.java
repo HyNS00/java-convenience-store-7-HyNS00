4f836565151854e0a5fc8aaa2635bed120e18305
Hyso <hsk7953@gmail.com>
2024-11-06 23:47:52 +0900

    feat : 리소스 파일 경로 관리를 위한 enum 추가

---
+++ src/main/java/store/ResourcePath.java
@@ -0,0 +1,16 @@
+package store;
+
+public enum ResourcePath {
+    PRODUCTS("products.md"),
+    PROMOTIONS("promotions.md");
+
+    private final String path;
+
+    ResourcePath(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
