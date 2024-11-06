773687767e8cf78fee0ce1868441eeb02383cc18
Hyso <hsk7953@gmail.com>
2024-11-06 23:50:20 +0900

    feat(Product) : Product 객체 생성 로직 추가 - 생성자를 private 으로 변경

--- src/main/java/store/model/Product.java
+++ src/main/java/store/model/Product.java
@@ -1,16 +1,28 @@
 package store.model;
 
+import static store.loader.PromotionLoader.findPromotion;
+
 public class Product {
     private final String name;
     private final int price;
     private  int quantity;
     private final Promotion promotion;
 
-    public Product(final String name, final int price, int quantity, final Promotion promotion) {
+    private Product(final String name, final int price, int quantity, final Promotion promotion) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.promotion = promotion;
+    }
+
+    public static Product createProduct(String line){
+        String[] parts = line.split(",");
+        String name = parts[0];
+        int price = Integer.parseInt(parts[1]);
+        int quantity = Integer.parseInt(parts[2]);
+        Promotion promotion = findPromotion(parts[3]);
+
+        return new Product(name,price,quantity,promotion);
     }
 
     public String getName() {
