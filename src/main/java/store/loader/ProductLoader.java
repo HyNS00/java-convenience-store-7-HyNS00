81d43e78613e4e76594d2e1c93e7fbf4cb866504
Hyso <hsk7953@gmail.com>
2024-11-07 21:55:31 +0900

    feat : 마크 다운으로부터 상품데이터를 파싱하는 ProductLoader 추가 - Loader 를 상속

---
+++ src/main/java/store/loader/ProductLoader.java
@@ -0,0 +1,29 @@
+package store.loader;
+
+import store.ResourcePath;
+import store.model.Product;
+import store.model.Products;
+import store.model.Promotions;
+
+import java.util.List;
+
+public class ProductLoader extends Loader<Products> {
+    private final Promotions promotions;
+
+    public ProductLoader(Promotions promotions) {
+        this.promotions = promotions;
+    }
+
+    @Override
+    public Products load() {
+        List<String> lines = loadFromMd(ResourcePath.PRODUCTS.getPath());
+        return parseProducts(lines);
+    }
+
+    private Products parseProducts(List<String> lines) {
+        List<Product> products = lines.stream().skip(1).filter(Product::isValidFormat)
+                .map(line -> Product.createProduct(line, promotions))
+                .toList();
+        return new Products(products);
+    }
+}
