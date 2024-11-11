5970e4381191673b95348bcf1de3a6f606817b4f
Hyso <hsk7953@gmail.com>
2024-11-11 17:21:35 +0900

    test : 물건품목을 가져오는 productLoader 단위 테스트 리팩토링 - ApplicationTest 와의 구분을 위해 products2로 이름 변경

--- src/test/java/store/loader/ProductLoaderTest.java
+++ src/test/java/store/loader/ProductLoaderTest.java
@@ -10,37 +10,57 @@ import store.model.Promotion;
 import store.model.Promotions;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 
 class ProductLoaderTest {
-    private ProductLoader productLoader;
     private Promotions promotions;
-    private PromotionLoader promotionLoader;
+    private Products testProducts;
+    private TestLoader productTestLoader;
+    private TestLoader promotionTestLoader;
 
     @BeforeEach
     void setup() {
-        promotionLoader = new PromotionLoader();
-        promotions = promotionLoader.load();
-        productLoader = new ProductLoader(promotions);
+        promotions = loadPromotions();
+        testProducts = loadProducts(promotions);
+
+    }
+
+    private Promotions loadPromotions() {
+        promotionTestLoader = new TestLoader("promotions2.md");
+        List<String> promotionLines = promotionTestLoader.load();
+        List<Promotion> promotionList = promotionLines.stream()
+                .skip(1)
+                .filter(Promotion::isValidFormat)
+                .map(Promotion::createPromotion)
+                .toList();
+        return new Promotions(promotionList);
+    }
+
+    private Products loadProducts(Promotions promotions) {
+        productTestLoader = new TestLoader("products2.md");
+        List<String> productLines = productTestLoader.load();
+        List<Product> productList = productLines.stream()
+                .skip(1)
+                .filter(Product::isValidFormat)
+                .map(line -> Product.createProduct(line, promotions))
+                .toList();
+        return new Products(productList);
     }
 
     @DisplayName("올바르게 파일 입출력을 통해 파일을 성공적으로 불러온 경우")
     @Test
     void readFile_success() {
-
-        Products loaded = productLoader.load();
-
-        assertThat(loaded).isNotNull();
-        assertThat(loaded.getProducts().size()).isEqualTo(2);
+        assertThat(testProducts).isNotNull();
+        assertThat(testProducts.getProducts()).hasSize(2);
     }
 
     @Test
     void readFile_success_exists_promotion() {
-        Products loaded = productLoader.load();
 
-        Product product = loaded.getProducts().get(0);
+        Product product = testProducts.getProducts().get(0);
         assertThat(product).extracting("name", "price", "quantity")
                 .containsExactly("오렌지주스", 1800, 9);
 
@@ -52,9 +72,7 @@ class ProductLoaderTest {
 
     @Test
     void readFile_success_nonExists_promotion() {
-        Products loaded = productLoader.load();
-
-        Product product = loaded.getProducts().get(1);
+        Product product = testProducts.getProducts().get(1);
 
         assertThat(product).extracting("name", "price", "quantity", "promotion")
                 .containsExactly("콜라", 1000, 10, null);
