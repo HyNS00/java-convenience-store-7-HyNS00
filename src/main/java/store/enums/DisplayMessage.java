a769979bb0483344189e804c7deb747ba75262e9
Hyso <hsk7953@gmail.com>
2024-11-11 19:28:56 +0900

    feat : 정적인 메시지들을 관리하는 enum 클래스 DisplayMessage 구현 - 관련 클래스 리팩토링

---
+++ src/main/java/store/enums/DisplayMessage.java
@@ -0,0 +1,19 @@
+package store.enums;
+
+public enum DisplayMessage {
+    WELCOME("\n안녕하세요. W편의점입니다."),
+    CURRENT_STOCK("현재 보유하고 있는 상품입니다.\n"),
+    STORE_HEADER("\n==============W 편의점================"),
+    ITEM_HEADER("상품명            수량         금액"),
+    BONUS_HEADER("=============증\t정==================\n"),
+    SEPARATOR_LINE("====================================");
+    private final String message;
+
+    DisplayMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,5 +1,7 @@
 package store.view;
 
+import store.enums.DisplayMessage;
+import store.enums.OutputMessage;
 import store.model.*;
 
 import java.util.List;
@@ -7,26 +9,26 @@ import java.util.Optional;
 
 public class OutputView {
 
-    public void welcomeMessage(Products products){
-        System.out.println("\n안녕하세요. W편의점입니다.");
-        System.out.println("현재 보유하고 있는 상품입니다.\n");
+    public void welcomeMessage(Products products) {
+        System.out.println(DisplayMessage.WELCOME.getMessage());
+        System.out.println(DisplayMessage.CURRENT_STOCK.getMessage());
         products.getProducts().forEach(this::printProduct);
     }
 
-    private void printProduct(Product product){
+    private void printProduct(Product product) {
         System.out.println(formatProduct(product));
     }
 
-    private String formatProduct(Product product){
+    private String formatProduct(Product product) {
         return String.format("- %s %,d원 %s%s",
                 product.getName(),
                 product.getPrice(),
                 formatQuantity(product.getQuantity()),
                 formatPromotion(product.getPromotion())
-                );
+        );
     }
 
-    private String formatQuantity(int quantity){
+    private String formatQuantity(int quantity) {
         if (quantity == 0) {
             return "재고 없음";
         }
@@ -40,28 +42,29 @@ public class OutputView {
         return " " + promotion.getName();
     }
 
-    public void printReceipt(Receipt receipt, List<OrderItem> orderItems,Products products){
+    public void printReceipt(Receipt receipt, List<OrderItem> orderItems, Products products) {
         generateStoreHeader();
         printPurchaseItems(orderItems, products);
         printBonusItem(orderItems);
-        System.out.println("====================================");
-        printTotalAmount(receipt,orderItems);
+        System.out.println(DisplayMessage.SEPARATOR_LINE.getMessage());
+        printTotalAmount(receipt, orderItems);
 
     }
+
     private void generateStoreHeader() {
-        System.out.println("\n==============W 편의점================");
-        System.out.println("상품명            수량         금액");
+        System.out.println(DisplayMessage.STORE_HEADER.getMessage());
+        System.out.println(DisplayMessage.ITEM_HEADER.getMessage());
 
     }
 
-    private void printPurchaseItems(List<OrderItem> orderItems,Products products){
-        for(OrderItem item : orderItems){
+    private void printPurchaseItems(List<OrderItem> orderItems, Products products) {
+        for (OrderItem item : orderItems) {
             int purchaseQuantity = item.getTotal();
             Product product = Optional.ofNullable(products.findPromotionProduct(item.getProductName()))
                     .orElseGet(() -> products.findNonPromotionalProduct(item.getProductName()));
             long totalAmount = (long) product.getPrice() * purchaseQuantity;
             if (purchaseQuantity > 0) {
-                System.out.printf("%-12s      %2d     %,8d%n",
+                System.out.printf("%-8s      %2d     %,8d%n",
                         item.getProductName(),
                         purchaseQuantity,
                         totalAmount);
@@ -69,11 +72,11 @@ public class OutputView {
         }
     }
 
-    private void printBonusItem(List<OrderItem> orderItems){
+    private void printBonusItem(List<OrderItem> orderItems) {
         boolean hasBonusItem = orderItems.stream()
                 .anyMatch(item -> item.getResult().getPromotionBonus() > 0);
         if (hasBonusItem) {
-            System.out.printf("=============증\t정===============%n");
+            System.out.printf(DisplayMessage.BONUS_HEADER.getMessage());
             orderItems.stream()
                     .filter(item -> item.getResult().getPromotionBonus() > 0)
                     .forEach(item -> System.out.printf("%-12s      %d%n",
@@ -93,21 +96,28 @@ public class OutputView {
                 receipt.getTotalAmount());
 
         if (receipt.getBonusDiscount() > 0) {
-            System.out.printf("행사할인                 %,8d%n",
+            System.out.printf("행사할인                 %+,8d%n",
                     -receipt.getBonusDiscount());
         }
 
+        if (receipt.getBonusDiscount() == 0) {
+            System.out.printf("행사할인                 %8s%n", "-0");
+        }
+
         if (receipt.getMembershipDiscount() > 0) {
-            System.out.printf("멤버십할인               %,8d%n",
+            System.out.printf("멤버십할인               %+,8d%n",
                     -receipt.getMembershipDiscount());
         }
 
+        if (receipt.getMembershipDiscount() == 0) {
+            System.out.printf("멤버십할인                 %8s%n", "-0");
+        }
         System.out.printf("내실돈                   %,8d\n",
                 receipt.getFinalAmount());
     }
 
     public void printErrorMessage(IllegalArgumentException exception) {
-        System.out.println(exception.getMessage());
+        System.out.println(OutputMessage.ERROR_PREFIX.getMessage() + exception.getMessage());
     }
 
 }
