782baa23f71528d636124f94163b9b55400f9196
Hyso <hsk7953@gmail.com>
2024-11-11 21:04:36 +0900

    refactor(Application) : Application 리팩토링 - Factory 클래스 도입 - StoreController 생성자 변화

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,30 +1,20 @@
 package store;
 
 import store.controller.StoreController;
-import store.loader.ProductLoader;
-import store.loader.PromotionLoader;
-import store.model.Products;
-import store.service.OrderService;
-import store.service.PaymentService;
-import store.service.PromotionService;
-import store.validator.InputValidator;
-import store.view.InputView;
-import store.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        PromotionLoader promotionLoader = new PromotionLoader();
-        ProductLoader productLoader = new ProductLoader(promotionLoader.load());
-        Products loaded = productLoader.load();
-        InputView inputView = new InputView(new InputValidator());
-        OutputView outputView = new OutputView();
-        OrderService orderService = new OrderService(loaded);
-        PromotionService promotionService = new PromotionService(loaded);
-        PaymentService paymentService = new PaymentService(loaded);
-        StoreController storeController = new StoreController(inputView,orderService,
-                promotionService,outputView,loaded,paymentService);
-
-        storeController.start();
+        runStore();
+    }
+    private static void runStore(){
+        try{
+            StoreController storeController = StoreFactory.create();
+            storeController.start();
+        }catch (IllegalArgumentException  e) {
+            System.err.println(e.getMessage());
+        }catch (RuntimeException e){
+            System.err.println(e.getMessage());
+        }
     }
 }
