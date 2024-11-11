5f43ea014619ff824e1b3059eef8b0efb5c7c87b
Hyso <hsk7953@gmail.com>
2024-11-11 18:42:22 +0900

    feat : 프로그램 구현 Application 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,30 @@
 package store;
 
+import store.controller.StoreController;
+import store.loader.ProductLoader;
+import store.loader.PromotionLoader;
+import store.model.Products;
+import store.service.OrderService;
+import store.service.PaymentService;
+import store.service.PromotionService;
+import store.validator.InputValidator;
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PromotionLoader promotionLoader = new PromotionLoader();
+        ProductLoader productLoader = new ProductLoader(promotionLoader.load());
+        Products loaded = productLoader.load();
+        InputView inputView = new InputView(new InputValidator());
+        OutputView outputView = new OutputView();
+        OrderService orderService = new OrderService(loaded);
+        PromotionService promotionService = new PromotionService(loaded);
+        PaymentService paymentService = new PaymentService(loaded);
+        StoreController storeController = new StoreController(inputView,orderService,
+                promotionService,outputView,loaded,paymentService);
+
+        storeController.start();
     }
 }
