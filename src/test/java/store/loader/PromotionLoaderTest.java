d73633a2ed32afb760dfcb310cecf8b22699d05f
Hyso <hsk7953@gmail.com>
2024-11-11 16:33:22 +0900

    test : 프로모션 로더 테스트 코드 리팩토링 - promotions2 으로 이름 변경

--- src/test/java/store/loader/PromotionLoaderTest.java
+++ src/test/java/store/loader/PromotionLoaderTest.java
@@ -9,22 +9,30 @@ import store.model.Promotion;
 import store.model.Promotions;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PromotionLoaderTest {
     private PromotionLoader promotionLoader;
-
+    private TestLoader testLoader;
     @BeforeEach
     void setup() {
         promotionLoader = new PromotionLoader();
+        testLoader = new TestLoader("promotions2.md");
     }
 
     @DisplayName("프로모션 파일에서 성공적으로 프로모션을 불러온 경우")
     @Test
     void readPromotion_success() {
-        Promotions promotions = promotionLoader.load();
-        Promotion promotion = promotions.findPromotion("테스트-프로모션");
+        List<String> testLines = testLoader.load();
+        List<Promotion> promotions = testLines.stream()
+                .skip(1)
+                .filter(Promotion::isValidFormat)
+                .map(Promotion::createPromotion)
+                .toList();
+        Promotions testPromotions = new Promotions(promotions);
+        Promotion promotion = testPromotions.findPromotion("테스트-프로모션");
 
         assertThat(promotions).isNotNull();
         assertThat(promotion).extracting("name", "buy", "get", "startDate", "endDate")
