1b34e56d8863acb4f70a94892719ee82fffcac0d
Hyso <hsk7953@gmail.com>
2024-11-11 19:55:55 +0900

    feat : 구매목록과 상품 목록을 관리하는 enum 클래스  OutputMessage 구현

---
+++ src/main/java/store/enums/OutputMessage.java
@@ -0,0 +1,26 @@
+package store.enums;
+
+public enum OutputMessage {
+    ERROR_PREFIX("[ERROR] "),
+    PRODUCT_FORMAT("- %s %,d원 %s%s"),
+    PRODUCT_QUANTITY("%d개"),
+    EMPTY(""),
+    WHITE_SPACE(" "),
+    PURCHASE_ORDER_ITEM_FORMAT("%-8s      %2d         %,8d%n"),
+    BONUS_ITEM_FORMAT("%-12s      %d%n"),
+    TOTAL_AMOUNT_FORMAT("총구매액          %2d       %,8d%n"),
+    FINAL_AMOUNT_FORMAT("내실돈                   %,8d\n"),
+    DISCOUNT_FORMAT("%-8s                 %+,8d%n"),
+    ZERO_DISCOUNT_FORMAT("%-8s                 %8s%n"),
+    DISCOUNT_ZERO("-0");
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
