350353127591b05815215d83851383cd5b216025
Hyso <hsk7953@gmail.com>
2024-11-11 20:09:41 +0900

    feat : 입력 메시지를 상수로 관리하는 enum 클래스 InputViewMessage 추가 - InputView 클래스 리팩토링

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,6 +1,7 @@
 package store.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.enums.InputMessage;
 import store.model.Product;
 import store.model.Promotion;
 import store.model.PromotionResult;
@@ -14,7 +15,7 @@ public class InputView {
     }
 
     public String requestOrder() {
-        System.out.println("\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        System.out.println(InputMessage.REQUEST_ORDER.getMessage());
         String input = Console.readLine();
         inputValidator.validateOrder(input);
         return input;
@@ -22,7 +23,7 @@ public class InputView {
 
     public String requestPlusOrder(Product product) {
         Promotion promotion = product.getPromotion();
-        System.out.printf("\n현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", product.getName());
+        System.out.printf(InputMessage.REQUEST_ORDER.getMessage(), product.getName());
         String input = Console.readLine();
         inputValidator.validateResponse(input);
         return input;
@@ -30,21 +31,21 @@ public class InputView {
 
     public String requestPartialPromotion(String name, PromotionResult result) {
         int partialPromo = result.getPartialPromo();
-        System.out.printf("\n현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n", name, partialPromo);
+        System.out.printf(InputMessage.REQUEST_PARTIAL_PROMOTION.getMessage(), name, partialPromo);
         String input = Console.readLine();
         inputValidator.validateResponse(input);
         return input;
     }
 
     public String requestMembershipOption() {
-        System.out.println("\n멤버십 할인을 받으시겠습니까? (Y/N)");
+        System.out.println(InputMessage.REQUEST_MEMBERSHIP_DISCOUNT.getMessage());
         String input = Console.readLine();
         inputValidator.validateResponse(input);
         return input;
     }
 
     public String requestAdditionalOrder() {
-        System.out.println("\n감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        System.out.println(InputMessage.REQUEST_START_AGAIN.getMessage());
         String input = Console.readLine();
         inputValidator.validateResponse(input);
         return input;
